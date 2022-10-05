fdb265bf8e526d439dcfc501de9714d075ae53ec
Stefan Kopco <hey@oddcelot.dev>
2022-10-05 21:42:17 +0200

    meta: remove default setup styles and assets from init
    defaults established in ccd9e7ad1af4d363604bd349fa1953a776a2d61e

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import styles from './styles/root.scss';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+    <div class={styles}>
+      <header>
       </header>
+      <main></main>
+      <footer></footer>
     </div>
   );
 };
