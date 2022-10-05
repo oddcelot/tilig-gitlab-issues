8cf67cd4ce9f1e46cd9812550fb5fa787891dadd
Stefan Kopco <hey@oddcelot.dev>
2022-10-05 23:53:26 +0200

    feat: setup issue fetcher with env variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,27 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import type { Component } from "solid-js";
+import { Component, createResource } from "solid-js";
 
 import "./styles/index.scss";
 import "./styles/app.scss";
 
 import Header from "./components/Header/Header";
 
+export const fetchIssues = async (token: string) =>
+  (
+    await fetch(`${import.meta.env.OPEN_API_URL}/issues`, {
+      headers: {
+        "PRIVATE-TOKEN": token,
+      },
+    })
+  ).json();
+
 const App: Component = () => {
+  const [issues, { mutate, refetch }] = createResource(
+    import.meta.env.OPEN_AUTH_TOKEN,
+    fetchIssues
+  );
+
   return (
     <>
       <Header />
--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,5 @@ export default defineConfig({
   build: {
     target: 'esnext',
   },
+  envPrefix: "OPEN_"
 });
